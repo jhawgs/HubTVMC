9ae0e60c22a0a28a4b1f8dab2faac1821ec2299b
jhawgs <nizzyb05@gmail.com>
2025-02-09 13:39:46 -0800

    add debug

--- app.py
+++ app.py
@@ -89,4 +89,4 @@ def handle_disconnect():
     OCCUPIED = False
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5022)
+    socketio.run(app, host='0.0.0.0', port=5022, debug=True)
