2a49f70b1ded7a612031d4ca2b6c8bc0cb136a1f
jhawgs <nizzyb05@gmail.com>
2025-02-09 13:24:13 -0800

    got buttons working

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # app.py (on Raspberry Pi)
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, request
 from flask_socketio import SocketIO
 import socket
 from time import time, sleep
@@ -19,6 +19,17 @@ else:
 
 app = Flask(__name__)
 socketio = SocketIO(app)
+
+def detect_mobile(user_agent):
+    try:
+        mobile_keywords = ['Android', 'iPhone', 'iPad', 'Windows Phone', 'Mobile']
+        for keyword in mobile_keywords:
+            if keyword.lower() in user_agent.lower():
+                return True
+        return False
+    except:
+        print("failed mobile detect attempt")
+        return False
 
 # IP and port of the remote machine to forward events
 #REMOTE_IP = '192.168.1.100'  # Change to the target machine's IP
@@ -42,14 +53,17 @@ def waiting_room():
 
 @app.route('/controller')
 def controller():
-   global REFRESH_TIME, OCCUPIED
-   print(OCCUPIED)
-   if OCCUPIED:
-       return redirect('/waiting-room')
-   OCCUPIED = True
-   print(OCCUPIED)
-   REFRESH_TIME = time()
-   return render_template('index.html')
+    global REFRESH_TIME, OCCUPIED
+    print(OCCUPIED)
+    if OCCUPIED:
+        return redirect('/waiting-room')
+    OCCUPIED = True
+    print(OCCUPIED)
+    REFRESH_TIME = time()
+    if detect_mobile(request.headers.get('User-Agent')):
+        return render_template('mobile_index.html')
+    else:
+        return render_template('index.html')
 
 @app.route('/exit')
 def done():
@@ -65,7 +79,7 @@ def override():
 
 @socketio.on('input_event')
 def handle_input_event(data):
-    #print(data)
+    print(data)
     handle_key(data)#print('Received:', data)
     #remote_socket.sendto(str(data).encode(), (REMOTE_IP, REMOTE_PORT))
 
