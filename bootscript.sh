8b54e2a508734fdc7e5d5d4139baf39c2027e831
jhawgs <nizzyb05@gmail.com>
2025-02-09 16:50:48 -0800

    bootscript

--- bootscript.sh
+++ bootscript.sh
@@ -3,15 +3,15 @@ echo Restarted  Webservice
 
 startx & echo Starting GUI
 
-wait 120
+#wait 120
 
-sudo xset dpms force off
+#sudo xset dpms force off
 
-wait 5
+#wait 5
 
-/home/HubTVMC/start.sh & echo Starting Control Server
-/home/scripts/minecraft.sh & echo Starting Minecraft
-
+#/home/HubTVMC/start.sh & echo Starting Control Server
+#/home/scripts/minecraft.sh & echo Starting Minecraft
+#______________________
 #/home/HubTVMC/start.sh
 #echo Started Control Server
 
